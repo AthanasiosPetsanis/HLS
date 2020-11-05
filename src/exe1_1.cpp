5efc17c5ebbe6e20b8b6967ac430a4ac263b7bc2
thanosthemadman <thanosthemadman@gmail.com>
2020-11-05 20:27:27 +0200

    Finished 1st exercise of set 1

---
+++ src/exe1_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+unsigned short int numof_1bits(unsigned int &var){
+	short int bits = 8*sizeof(var);
+	std::cout << "The size of the variable you entered is: " << bits << " bits." << std::endl;
+	unsigned int div;
+	unsigned short int counter = 0;
+	for (int i = bits; i > 0; --i) {
+		div = var/pow(2,i-1);
+		counter += div;
+		var -= div*pow(2,i-1);
+		std::cout << "The div result is: " << div << "." << std::endl;
+		std::cout << "The counter value is: " << counter << "." << std::endl;
+	}
+	return counter;
+}
+
+int main(){
+	unsigned int var = 19;
+	unsigned short int result = numof_1bits(var);
+	std::cout << "The number of 1s in the bit sequence of the number is: " << var << " is: " << result << std::endl;
+	return 0;
+}
